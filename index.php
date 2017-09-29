e86117d7406fe2faba9656d8078673e4bef43ce9
USP <USP@DESKTOP-TNO3FK2>
2017-09-29 14:54:21 -0500

    avance de la primera parte

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+class semana07{
+function mayorDosnumeros($n1,$n2)
+{
+    if ($n1>$n2)
+       return $n1;
+    
+    else 
+        return $n2;
+    
+}
+function menorDosenteros()
+{return 10; }
+
+}
+
+
